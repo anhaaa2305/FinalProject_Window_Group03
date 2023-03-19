940b35313ec22db7cec161855bbc24f95ef6694d
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-03-19 13:25:54 +0700

    refactor(NavigationService): inject service provider

--- WpfApp1/Services/NavigationService.cs
+++ WpfApp1/Services/NavigationService.cs
@@ -5,10 +5,17 @@ namespace WpfApp1.Services;
 
 public class NavigationService : INavigationService
 {
+	private readonly IServiceProvider provider;
+
+	public NavigationService(IServiceProvider provider)
+	{
+		this.provider = provider;
+	}
+
 	public event Action<UserControl>? Navigated;
 	public bool Navigate<T>() where T : UserControl
 	{
-		var service = App.Current.Services.GetService<T>();
+		var service = provider.GetService<T>();
 		if (service is null)
 		{
 			return false;
