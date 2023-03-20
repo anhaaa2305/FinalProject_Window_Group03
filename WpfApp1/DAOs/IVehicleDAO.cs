f4c7d901ea7c5bc1bd6d360593dd8da9519b0032
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-03-20 16:33:43 +0700

    feat(VehicleDAO): add get overloads; ensure rented and reserved vehicles table

--- WpfApp1/DAOs/IVehicleDAO.cs
+++ WpfApp1/DAOs/IVehicleDAO.cs
@@ -4,6 +4,8 @@ namespace WpfApp1.DAOs;
 
 public interface IVehicleDAO : IBaseDAO<VehicleModel>
 {
+	Task<IReadOnlyCollection<VehicleModel>> GetAllRentedAsync(UserModel user);
 	Task<IReadOnlyCollection<VehicleModel>> GetAllRentedAsync();
+	Task<IReadOnlyCollection<VehicleModel>> GetAllReservedAsync(UserModel user);
 	Task<IReadOnlyCollection<VehicleModel>> GetAllReservedAsync();
 }
--- WpfApp1/DAOs/VehicleDAO.cs
+++ WpfApp1/DAOs/VehicleDAO.cs
@@ -1,6 +1,8 @@
 using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
+using System.Windows;
+using Microsoft.Data.SqlClient;
 using WpfApp1.Models;
 using WpfApp1.Services;
 
@@ -19,15 +21,33 @@ public class VehicleDAO : IVehicleDAO
 	{
 		try
 		{
-			await using var conn = await db.OpenAsync().ConfigureAwait(false);
-			if (conn is null)
-			{
-				return false;
-			}
+			var oks = await Task.WhenAll
+			(
+				EnsureVehicleTableAsync(),
+				EnsureRentedVehicleTableAsync(),
+				EnsureReservedVehicleTableAsync()
+			).ConfigureAwait(false);
+			return oks.All(ok => ok);
+		}
+		catch (Exception ex)
+		{
+			MessageBox.Show(ex.Message);
+			Debug.WriteLine(ex);
+			return false;
+		}
+	}
 
-			using var cmd = conn.CreateCommand();
-			cmd.CommandText =
-			@"
+	private async Task<bool> EnsureVehicleTableAsync()
+	{
+		await using var conn = await db.OpenAsync().ConfigureAwait(false);
+		if (conn is null)
+		{
+			return false;
+		}
+
+		using var cmd = conn.CreateCommand();
+		cmd.CommandText =
+		@"
 			if not exists (select * from sysobjects where name='Vehicles' and xtype='U')
 			create table Vehicles(
 				Id int not null identity(1, 1) primary key,
@@ -38,18 +58,66 @@ public class VehicleDAO : IVehicleDAO
 				ImageUrl varchar(256) null
 			)
 			";
-			// for (var i = 0; i != 100; ++i)
-			// {
-			// 	await AddAsync(new VehicleModel
-			// 	{
-			// 		LicensePlate = $"86-B1-{35904 + i}",
-			// 		Name = "Yamaha Sirius",
-			// 		PricePerDay = 120_000,
-			// 	});
-			// }
-			await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+		// for (var i = 0; i != 100; ++i)
+		// {
+		// 	await AddAsync(new VehicleModel
+		// 	{
+		// 		LicensePlate = $"86-B1-{35904 + i}",
+		// 		Name = "Yamaha Sirius",
+		// 		PricePerDay = 120_000,
+		// 	});
+		// }
+		await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+		return true;
+	}
+
+	private async Task<bool> EnsureRentedVehicleTableAsync()
+	{
+		await using var conn = await db.OpenAsync().ConfigureAwait(false);
+		if (conn is null)
+		{
+			return false;
 		}
-		catch (Exception ex) { Debug.WriteLine(ex); }
+
+		using var cmd = conn.CreateCommand();
+		cmd.CommandText =
+		@"
+			if not exists (select * from sysobjects where name='RentedVehicles' and xtype='U')
+			create table RentedVehicles(
+				Id int not null primary key,
+				UserId int not null,
+				StartDate datetime not null,
+				EndDate datetime not null,
+				constraint FK_RentedVehicles_Vehicles foreign key (Id) references Vehicles(Id) on update cascade on delete cascade,
+				constraint FK_RentedVehicles_Users foreign key (UserId) references Users(Id) on update cascade on delete cascade
+			)
+			";
+		await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+		return true;
+	}
+
+	private async Task<bool> EnsureReservedVehicleTableAsync()
+	{
+		await using var conn = await db.OpenAsync().ConfigureAwait(false);
+		if (conn is null)
+		{
+			return false;
+		}
+
+		using var cmd = conn.CreateCommand();
+		cmd.CommandText =
+		@"
+			if not exists (select * from sysobjects where name='ReservedVehicles' and xtype='U')
+			create table ReservedVehicles(
+				Id int not null primary key,
+				UserId int not null,
+				StartDate datetime not null,
+				EndDate datetime not null,
+				constraint FK_ReservedVehicles_Vehicles foreign key (Id) references Vehicles(Id) on update cascade on delete cascade,
+				constraint FK_ReservedVehicles_Users foreign key (UserId) references Users(Id) on update cascade on delete cascade
+			)
+			";
+		await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
 		return true;
 	}
 
@@ -165,7 +233,45 @@ public class VehicleDAO : IVehicleDAO
 		return await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
 	}
 
-	public async Task<IReadOnlyCollection<VehicleModel>> GetAllRentedAsync()
+	public Task<IReadOnlyCollection<VehicleModel>> GetAllRentedAsync()
+	{
+		return GetAllRentedAsync(
+		@"
+			select * from RentedVehicles
+			inner join Vehicles on RentedVehicles.Id = Vehicles.Id
+		");
+	}
+
+	public Task<IReadOnlyCollection<VehicleModel>> GetAllRentedAsync(UserModel user)
+	{
+		return GetAllRentedAsync(
+		@"
+			select * from RentedVehicles
+			where UserId = @Id
+			inner join Vehicles on RentedVehicles.Id = Vehicles.Id
+		", new SqlParameter("@Id", user.Id));
+	}
+
+	public Task<IReadOnlyCollection<VehicleModel>> GetAllReservedAsync()
+	{
+		return GetAllReservedAsync(
+		@"
+			select * from ReservedVehicles
+			inner join Vehicles on ReservedVehicles.Id = Vehicles.Id
+		");
+	}
+
+	public Task<IReadOnlyCollection<VehicleModel>> GetAllReservedAsync(UserModel user)
+	{
+		return GetAllRentedAsync(
+		@"
+			select * from ReservedVehicles
+			where UserId = @Id
+			inner join Vehicles on RentedVehicles.Id = Vehicles.Id
+		", new SqlParameter("@Id", user.Id));
+	}
+
+	private async Task<IReadOnlyCollection<VehicleModel>> GetAllReservedAsync(string commandText, params SqlParameter[] parameters)
 	{
 		await using var conn = await db.OpenAsync().ConfigureAwait(false);
 		if (conn is null)
@@ -174,16 +280,13 @@ public class VehicleDAO : IVehicleDAO
 		}
 
 		using var cmd = conn.CreateCommand();
-		cmd.CommandText =
-		@"
-			select * from RentedVehicles
-			inner join Vehicles on RentedVehicles.Id = Vehicles.Id
-		";
+		cmd.CommandText = commandText;
+		cmd.Parameters.AddRange(parameters);
 		using var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false);
 		var models = new LinkedList<VehicleModel>();
 		while (await reader.ReadAsync().ConfigureAwait(false))
 		{
-			var model = new RentedVehicleModel
+			var model = new ReservedVehicleModel
 			{
 				UserId = reader.GetInt32("UserId"),
 				StartDate = reader.GetDateTime("StartDate"),
@@ -195,7 +298,7 @@ public class VehicleDAO : IVehicleDAO
 		return models;
 	}
 
-	public async Task<IReadOnlyCollection<VehicleModel>> GetAllReservedAsync()
+	private async Task<IReadOnlyCollection<VehicleModel>> GetAllRentedAsync(string commandText, params SqlParameter[] parameters)
 	{
 		await using var conn = await db.OpenAsync().ConfigureAwait(false);
 		if (conn is null)
@@ -204,16 +307,13 @@ public class VehicleDAO : IVehicleDAO
 		}
 
 		using var cmd = conn.CreateCommand();
-		cmd.CommandText =
-		@"
-			select * from ReservedVehicles
-			inner join Vehicles on ReservedVehicles.Id = Vehicles.Id
-		";
+		cmd.CommandText = commandText;
+		cmd.Parameters.AddRange(parameters);
 		using var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false);
 		var models = new LinkedList<VehicleModel>();
 		while (await reader.ReadAsync().ConfigureAwait(false))
 		{
-			var model = new ReservedVehicleModel
+			var model = new RentedVehicleModel
 			{
 				UserId = reader.GetInt32("UserId"),
 				StartDate = reader.GetDateTime("StartDate"),
