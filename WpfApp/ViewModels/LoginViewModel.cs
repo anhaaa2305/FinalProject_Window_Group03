795dce54bad9b89c8c721dfb8d9639329fac92d6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-03-23 09:55:34 +0700

    feat(LoginViewModel): implement login

--- WpfApp/ViewModels/LoginViewModel.cs
+++ WpfApp/ViewModels/LoginViewModel.cs
@@ -1,15 +1,23 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
+using WpfApp.Data.Context;
 using WpfApp.Services;
 using WpfApp.Views.User;
 
 namespace WpfApp.ViewModels;
 
+using BCrypt.Net;
+using WpfApp.Data.Models;
+
 public class LoginViewModel : ObservableObject
 {
+	private readonly INavigationService navigationService;
+	private readonly IDbContextFactory<AppDbContext> dbContextFactory;
+	private readonly ISessionService sessionService;
 	private string username = string.Empty;
 	private string password = string.Empty;
-	public IRelayCommand LoginCommand { get; }
+	public IAsyncRelayCommand LoginCommand { get; }
 
 	public string Username
 	{
@@ -35,15 +43,36 @@ public class LoginViewModel : ObservableObject
 		}
 	}
 
-	private readonly INavigationService navigationService;
-	public LoginViewModel(INavigationService navigationService)
+	public LoginViewModel(INavigationService navigationService, IDbContextFactory<AppDbContext> dbContextFactory, ISessionService sessionService)
 	{
 		this.navigationService = navigationService;
-		LoginCommand = new RelayCommand(Login, CanLogin);
+		this.dbContextFactory = dbContextFactory;
+		this.sessionService = sessionService;
+		LoginCommand = new AsyncRelayCommand(LoginAsync, CanLogin);
 	}
 
-	private void Login()
+	private async Task LoginAsync()
 	{
+		using var ctx = dbContextFactory.CreateDbContext();
+		var user = await ctx.Users
+			.Where(e => e.FullName == Username)
+			.Select(e => new User { Id = e.Id, FullName = e.FullName, Password = e.Password })
+			.FirstOrDefaultAsync()
+			.ConfigureAwait(false);
+
+		if (user is null)
+		{
+			return;
+		}
+
+		var ok = await Task.Run(() => BCrypt.EnhancedVerify(Password, user.Password)).ConfigureAwait(false);
+		if (!ok)
+		{
+			return;
+		}
+
+		user.Password = string.Empty;
+		await sessionService.LogInAsync(user).ConfigureAwait(false);
 		navigationService.Navigate<HomeView>();
 	}
 
