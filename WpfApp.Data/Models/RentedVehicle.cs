1d19aadfaa4144dc62bfeeb64b93450e2dfff7ad
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-03-23 23:01:32 +0700

    refactor(Models): add User.IsMale; initialize default objects

--- WpfApp.Data/Models/RentedVehicle.cs
+++ WpfApp.Data/Models/RentedVehicle.cs
@@ -2,8 +2,8 @@ namespace WpfApp.Data.Models;
 
 public class RentedVehicle
 {
-	public User User { get; set; } = default!;
-	public Vehicle Vehicle { get; set; } = default!;
+	public User User { get; set; } = new();
+	public Vehicle Vehicle { get; set; } = new();
 	public DateTime StartDate { get; set; }
 	public DateTime EndDate { get; set; }
 }
--- WpfApp.Data/Models/User.cs
+++ WpfApp.Data/Models/User.cs
@@ -7,6 +7,7 @@ public class User
 	public string Password { get; set; } = string.Empty;
 	public string FullName { get; set; } = string.Empty;
 	public string PhoneNumber { get; set; } = string.Empty;
+	public bool IsMale { get; set; }
 	public string? Address { get; set; }
 	public string? Email { get; set; }
 	public DateTime? DateOfBirth { get; set; }
