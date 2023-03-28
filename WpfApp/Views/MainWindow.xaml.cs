57ac93561c6516822680e52590ce882f104fac8c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-03-28 21:27:05 +0700

    refactor: add roles dev database

--- WpfApp/Views/MainWindow.xaml.cs
+++ WpfApp/Views/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using Wpf.Ui.Appearance;
+﻿using AsyncAwaitBestPractices;
+using Wpf.Ui.Appearance;
 using Wpf.Ui.Contracts;
 using Wpf.Ui.Controls.Navigation;
 using Wpf.Ui.Controls.Window;
+using WpfApp.Data.Constants;
 using WpfApp.Data.DAOs;
 using WpfApp.Data.Models;
 using WpfApp.Services;
@@ -11,7 +13,7 @@ namespace WpfApp.Views;
 
 public partial class MainWindow : INavigationWindow
 {
-	public MainWindow(MainWindowViewModel vm, IPageService pageService, IAppNavigationService navigationService, IVehicleDAO vehicleDAO, IUserDAO userDAO)
+	public MainWindow(MainWindowViewModel vm, IPageService pageService, IAppNavigationService navigationService, IVehicleDAO vehicleDAO, IUserDAO userDAO, IRoleDAO roleDAO)
 	{
 		InitializeComponent();
 
@@ -20,26 +22,34 @@ public partial class MainWindow : INavigationWindow
 		navigationService.SetNavigationControl(RootNavigation);
 
 		Loaded += OnLoad;
+		EnsureDevelopmentDatabase(userDAO, roleDAO, vehicleDAO).SafeFireAndForget();
+	}
 
-		Task.Run(async () =>
+	private static async Task EnsureDevelopmentDatabase(IUserDAO userDAO, IRoleDAO roleDAO, IVehicleDAO vehicleDAO)
+	{
+		await userDAO.AddAsync(new Data.Models.User
 		{
-			await userDAO.AddAsync(new Data.Models.User
-			{
-				FullName = "user",
-				Password = "user"
-			});
-			for (var i = 0; i != 50; ++i)
-			{
-				await vehicleDAO.AddAsync(new Vehicle
-				{
-					Brand = "Yamaha",
-					Name = "Yamaha Sirius",
-					LicensePlate = "86-B1-" + (35904 + i),
-					PricePerDay = 100_000 + (int)Random.Shared.NextInt64(300_000),
-					ImageUrl = "http://yamaha-motor.com.vn/wp/wp-content/uploads/2017/05/500x400-Si-RC-trang-den-04.png"
-				});
-			}
+			FullName = "user",
+			Password = "user"
 		});
+		var id = await userDAO.AddAsync(new Data.Models.User
+		{
+			FullName = "staff",
+			Password = "staff"
+		});
+		await roleDAO.AddAsync(new Role { Flag = RoleFlag.Staff, Name = "Staff" });
+		await userDAO.AddUserRoleAsync(id, RoleFlag.Staff);
+		for (var i = 0; i != 25; ++i)
+		{
+			await vehicleDAO.AddAsync(new Vehicle
+			{
+				Brand = "Yamaha",
+				Name = "Yamaha Sirius",
+				LicensePlate = "86-B1-" + (35904 + i),
+				PricePerDay = 100_000 + (int)Random.Shared.NextInt64(300_000),
+				ImageUrl = "http://yamaha-motor.com.vn/wp/wp-content/uploads/2017/05/500x400-Si-RC-trang-den-04.png"
+			});
+		}
 	}
 
 	public void CloseWindow()
